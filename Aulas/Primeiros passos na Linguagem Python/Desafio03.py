81ccd2dbb8c184cc95b4cdf9f8edee5f03e3b193
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-03-09 20:41:55 -0300

    Include exercises

---
+++ Aulas/Primeiros passos na Linguagem Python/Desafio03.py
@@ -0,0 +1,7 @@
+#Fase 01: Desafio 3 - Mostra soma de dois números
+print('======== Desafio 03 ========')
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+soma = n1 + n2
+msg = 'A soma entre: {0} e {1} = {2}'.format(n1, n2, soma)
+print(msg)
